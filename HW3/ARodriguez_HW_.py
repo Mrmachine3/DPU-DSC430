0667c124aa20e2f488d07fd2236ed30ca7c5c064
mrmachine <atone36@gmail.com>
2024-01-30 12:48:13 -0600

    update template for py scripts

---
+++ HW3/ARodriguez_HW_.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+################################ Honor Statement ####################################
+#    I have not given or received any unauthorized assistance on this assignment.
+#####################################################################################
+# Author: Anthony M. Rodriguez
+# Date: 01/30/2024
+# Usage: /usr/bin/python3 ARodriguez_HW_.py
+# Alternate Python Usage: /usr/bin/python3 -m ARodriguez_HW_
+# Alternate CLI Usage: ./ARodriguez_HW_.py
+# Git Repo URL: https://github.com/Mrmachine3/DPU-DSC430.git
+# Video Explanation URL:
+#
+# Description:
+
+
+# LIBRARIES
+
+# FUNCTIONS
+
+# MAIN PROGRAM FUNCTION
+def main(name):
+    print(f"Hello {name}!")
+
+
+# MAIN PROGRAM INVOCATION
+if __name__ == "__main__":
+    main("Anthony")
